641379103c3679a340ae3ec8e7dd930df1897f89
dsusviela <d.susviela@gmail.com>
2021-01-21 16:39:16 -0300

    Connect preload and delete buttons to backend

--- src/components/SidePanel/Panels/GeneralSidePanel.js
+++ src/components/SidePanel/Panels/GeneralSidePanel.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button } from 'reactstrap';
+import { Button, Spinner } from 'reactstrap';
 import './GeneralSidePanel.css';
 import NewBeachSensorMenu from './GeneralSubmenus/NewBeachSensorMenu';
 import SimulationMenu from './GeneralSubmenus/SimulationMenu';
@@ -16,7 +16,23 @@ const GeneralSidePanel = ({
   createBusSensor
 }) => {
   const [ submenu, setSubmenu ] = useState('none');
+  const [ apiLoading, setApiLoading ] = useState(false);
   const { REACT_APP_SENSOR_BACKEND_API } = process.env;
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const preloadData = async (endpoint) => {
+    console.log('preloading data...');
+    setApiLoading(true);
+    axios
+      .post(`${REACT_APP_SENSOR_BACKEND_API}/${endpoint}/preload_data`)
+      .then((_res) => {
+        refreshPage();
+      })
+      .catch((error) => console.log(error));
+  };
 
   const renderSubMenu = () => {
     if (submenu === 'new-beach-sensor') {
@@ -42,79 +58,94 @@ const GeneralSidePanel = ({
     }
   };
 
-  const refreshPage = () => {
-    window.location.reload();
-  };
-
   const deleteAllSensors = () => {
-    axios
-      .delete(`${REACT_APP_SENSOR_BACKEND_API}/bus_sensors/`)
-      .then(axios.delete(`${REACT_APP_SENSOR_BACKEND_API}/beach_sensors/`).then(refreshPage()));
+    setApiLoading(true);
+    Promise.all([
+      axios.delete(`${REACT_APP_SENSOR_BACKEND_API}/bus_sensors/`).catch((error) => {
+        console.log(error);
+      }),
+      axios.delete(`${REACT_APP_SENSOR_BACKEND_API}/beach_sensors/`).catch((error) => {
+        console.log(error);
+      })
+    ]).then(() => {
+      setApiLoading(false);
+      refreshPage();
+    });
   };
 
-  return (
-    <div className="general-panel">
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setSubmenu('simulation');
-        }}
-      >
-        Simular
-      </Button>
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setSubmenu('new-beach-sensor');
-        }}
-      >
-        Crear sensor de playas
-      </Button>
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setLocationMarker([]);
-          setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
-          setSubmenu('new-bus-sensor');
-        }}
-      >
-        Crear sensor omnibus
-      </Button>
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setLocationMarker([]);
-          setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
-          setSubmenu('none');
-        }}
-      >
-        Precargar datos de playas
-      </Button>
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setLocationMarker([]);
-          setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
-          setSubmenu('none');
-        }}
-      >
-        Precargar datos de omnibus
-      </Button>
-      <Button
-        className="general-main-tab--button"
-        onClick={() => {
-          setLocationMarker([]);
-          setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
-          setSubmenu('none');
-          deleteAllSensors();
-        }}
-      >
-        Limpiar los sensores
-      </Button>
-      <hr />
-      {renderSubMenu()}
-    </div>
-  );
+  if (apiLoading) {
+    return (
+      <div className="activity-indicator">
+        <Spinner color="primary" />;
+      </div>
+    );
+  } else {
+    return (
+      <div className="general-panel">
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setSubmenu('simulation');
+          }}
+        >
+          Simular
+        </Button>
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setSubmenu('new-beach-sensor');
+          }}
+        >
+          Crear sensor de playas
+        </Button>
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setLocationMarker([]);
+            setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
+            setSubmenu('new-bus-sensor');
+          }}
+        >
+          Crear sensor omnibus
+        </Button>
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setLocationMarker([]);
+            setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
+            setSubmenu('none');
+            preloadData('beach_sensors');
+          }}
+        >
+          Precargar datos de playas
+        </Button>
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setLocationMarker([]);
+            setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
+            setSubmenu('none');
+            preloadData('bus_sensors');
+          }}
+        >
+          Precargar datos de omnibus
+        </Button>
+        <Button
+          className="general-main-tab--button"
+          onClick={() => {
+            setLocationMarker([]);
+            setNewBeachSensorData({ ...newBeachSensorData, ...{ listeningForLocation: false } });
+            setSubmenu('none');
+            deleteAllSensors();
+          }}
+        >
+          Limpiar los sensores
+        </Button>
+        <hr />
+        {renderSubMenu()}
+      </div>
+    );
+  }
 };
 
 export default GeneralSidePanel;
